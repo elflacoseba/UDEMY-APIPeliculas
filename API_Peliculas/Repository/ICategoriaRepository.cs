9e33a6b3bbbaeefd63620debf86df6de0f892ab5
Sebastián Serrisuela <sebaserri@gmail.com>
2021-12-16 14:10:25 -0300

    Creaada la interfaz ICategoriaRepository

---
+++ API_Peliculas/Repository/ICategoriaRepository.cs
@@ -0,0 +1,27 @@
+﻿using API_Peliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Peliculas.Repository
+{
+    public interface ICategoriaRepository
+    {
+        ICollection<Categoria> GetCategorias();
+
+        Categoria GetCategoria(int id);
+
+        bool ExistsCategoria(string name);
+
+        bool ExistsCategoria(int id);
+
+        bool CreateCategoria(Categoria categoria);
+
+        bool UpdateCategoria(Categoria categoria);
+
+        bool DeleteCategoria(int id);
+
+        bool Save();
+    }
+}
