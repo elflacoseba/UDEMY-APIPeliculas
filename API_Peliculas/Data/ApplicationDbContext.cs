d0933f183a9da4177adfd96c53b8cca7bccc9e9f
Sebastián Serrisuela <sebaserri@gmail.com>
2021-12-16 12:28:20 -0300

    Creada la clase ApplicationDbContext, agregadas las referencias a EntityFrameworkCore y creada la migración inicial.

---
+++ API_Peliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API_Peliculas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Peliculas.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
