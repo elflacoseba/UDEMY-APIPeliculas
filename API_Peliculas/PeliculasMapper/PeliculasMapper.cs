f8c0a0d98c39b2935c1d5319a36d5de87f4fb5c7
Sebastián Serrisuela <sebaserri@gmail.com>
2021-12-17 14:23:23 -0300

    Agregadas referencias a Automapper y creada clase PeliculasMapper con el mapeo entre Categoria y CategoriaDTO.

---
+++ API_Peliculas/PeliculasMapper/PeliculasMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Peliculas.Models;
+using API_Peliculas.Models.Dtos;
+using AutoMapper;
+
+namespace API_Peliculas.PeliculasMapper
+{
+    public class PeliculasMapper : Profile
+    {
+        public PeliculasMapper()
+        {
+            CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+        }
+    }
+}
