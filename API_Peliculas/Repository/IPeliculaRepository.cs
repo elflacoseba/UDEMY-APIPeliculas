3020b0be5813605e36c60175bad930bceb5e825e
Sebastián Serrisuela <sebaserri@gmail.com>
2021-12-20 09:31:04 -0300

    Creada la interfaz y la implementación de Películas en el repositorio.

---
+++ API_Peliculas/Repository/IPeliculaRepository.cs
@@ -0,0 +1,27 @@
+﻿using API_Peliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Peliculas.Repository
+{
+    public interface IPeliculaRepository
+    {
+        ICollection<Pelicula> GetPeliculas();
+
+        Pelicula GetPelicula(int id);
+
+        bool ExistsPelicula(string name);
+
+        bool ExistsPelicula(int id);
+
+        bool CreatePelicula(Pelicula pelicula);
+
+        bool UpdatePelicula(Pelicula pelicula);
+
+        bool DeletePelicula(int id);
+
+        bool Save();
+    }
+}
---
+++ API_Peliculas/Repository/PeliculaRepository.cs
@@ -0,0 +1,72 @@
+﻿using API_Peliculas.Data;
+using API_Peliculas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Peliculas.Repository
+{
+    public class PeliculaRepository : IPeliculaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PeliculaRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreatePelicula(Pelicula pelicula)
+        {
+            _db.Pelicula.Add(pelicula);
+            return Save();
+        }
+
+        public bool DeletePelicula(int id)
+        {
+            Pelicula peli = _db.Pelicula.FirstOrDefault(c => c.Id == id);
+
+            if (peli != null)
+            {
+                _db.Pelicula.Remove(peli);
+                return Save();
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public bool ExistsPelicula(string name)
+        {
+            return _db.Pelicula.Any(c => c.Nombre.Trim().ToLower() == name.Trim().ToLower());
+        }
+
+        public bool ExistsPelicula(int id)
+        {
+            return _db.Pelicula.Any(c => c.Id == id);
+        }
+
+        public Pelicula GetPelicula(int id)
+        {
+            return _db.Pelicula.FirstOrDefault(c => c.Id == id);
+        }
+
+        public ICollection<Pelicula> GetPeliculas()
+        {
+            return _db.Pelicula.ToList();
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdatePelicula(Pelicula pelicula)
+        {
+            _db.Pelicula.Update(pelicula);
+            return Save();
+        }
+    }
+}
