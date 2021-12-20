3eb1275a7a3a2da7145439d5ead75f3edd07b321
Sebastián Serrisuela <sebaserri@gmail.com>
2021-12-20 09:25:08 -0300

    Creación de los DTOs para la Película

---
+++ API_Peliculas/Models/Dtos/PeliculaCreateDTO.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static API_Peliculas.Models.Pelicula;
+
+namespace API_Peliculas.Models.Dtos
+{
+    public class PeliculaCreateDTO
+    {
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        public IFormFile Foto { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La duración es obligatoria.")]
+        public int Duracion { get; set; }
+
+        public TipoClasificacion Clasificacion { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public int CategoriaID { get; set; }
+    }
+}
---
+++ API_Peliculas/Models/Dtos/PeliculaDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static API_Peliculas.Models.Pelicula;
+
+namespace API_Peliculas.Models.Dtos
+{
+    public class PeliculaDTO
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string Nombre { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La duración es obligatoria.")]
+        public int Duracion { get; set; }
+
+        public TipoClasificacion Clasificacion { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public Categoria Categoria { get; set; }
+    }
+}
